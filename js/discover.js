6520d7cf61a5d519fcc5d66f3dfefc01ee187c06
Batool Malkawi <batoolmalkawii@gmail.com>
2020-09-28 11:14:02 +0300

    Discover CSS

--- js/discover.js
+++ js/discover.js
@@ -17,64 +17,32 @@ function getFromLocalStorage() {
 }
 
 function showDiscover() {
-  for (let i = 0; i < OffersInfo.length; i++) {
-    let thEl = document.createElement('th');
-    tableEl.appendChild(thEl);
-    thEl.textContent = OffersInfo[i];
-  }
   renderOfferRows(existingOfferLocalStorage);
   renderOfferRows(RideOffer.all);
 }
 
-function renderOfferRows(offers){
+function renderOfferRows(offers) {
   for (let i = 0; i < offers.length; i++) {
     let trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
-    let tdELImg = document.createElement('td');
-    trEl.appendChild(tdELImg);
-    let imgEl = document.createElement('img');
-    tdELImg.appendChild(imgEl);
-    imgEl.src = offers[i].path;
-    let tdEl1 = document.createElement('td');
-    trEl.appendChild(tdEl1);
-    tdEl1.textContent = offers[i].name;
-    let tdEl2 = document.createElement('td');
-    trEl.appendChild(tdEl2);
-    tdEl2.textContent = offers[i].from;
-    let tdEl3 = document.createElement('td');
-    trEl.appendChild(tdEl3);
-    tdEl3.textContent = offers[i].to;
-    let tdEl4 = document.createElement('td');
-    trEl.appendChild(tdEl4);
-    tdEl4.textContent = offers[i].day;
-    let tdEl5 = document.createElement('td');
-    trEl.appendChild(tdEl5);
-    tdEl5.textContent = offers[i].time;
-    let tdEl6 = document.createElement('td');
-    trEl.appendChild(tdEl6);
-    let tdEl7 = document.createElement('td');
-    trEl.appendChild(tdEl7);
-    tdEl7.textContent = offers[i].payment;
-    let tdEl8 = document.createElement('td');
-    trEl.appendChild(tdEl8);
-    tdEl8.textContent = offers[i].cost;
-    let tdEl9 = document.createElement('td');
-    trEl.appendChild(tdEl9);
-    tdEl9.textContent = offers[i].phone;
-    let tdEl10 = document.createElement('td');
-    trEl.appendChild(tdEl10);
-    tdEl10.textContent = offers[i].email;
-    let tdEl11 = document.createElement('td');
-    trEl.appendChild(tdEl11);
-    tdEl11.innerHTML = offers[i].fromLocation;
-    let tdEl12 = document.createElement('td');
-    trEl.appendChild(tdEl12);
-    tdEl12.innerHTML = offers[i].toLocation;
+    let tdElImg = document.createElement('img');
+    trEl.appendChild(tdElImg);
+    tdElImg.src = offers[i].path;
+    let tdElInfo = document.createElement('td');
+    trEl.appendChild(tdElInfo);
+    tdElInfo.textContent = `${offers[i].name} is going from ${offers[i].from} to ${offers[i].to} on ${offers[i].day} at ${offers[i].time}.
+    The ride with ${offers[i].name} costs ${offers[i].cost} and the accepted payment method is ${offers[i].payment} payment. Call at ${offers[i].phone} or contact ${offers[i].email}  `;
+    let tdElFromMap = document.createElement('td');
+    trEl.appendChild(tdElFromMap);
+    tdElFromMap.innerHTML = offers[i].fromLocation;
+    let tdElToMap = document.createElement('td');
+    trEl.appendChild(tdElToMap);
+    tdElToMap.innerHTML = offers[i].toLocation;
   }
 }
 
-function setDefaultImg(offers){
-  for (let i= 0; i<offers.length;i++){
+function setDefaultImg(offers) {
+  for (let i = 0; i < offers.length; i++) {
     offers[i].path = 'img/default.jpg';
   }
   return (offers);

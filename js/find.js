d3098655c5761991fe930e7c61f20f6b4e900565
Batool Malkawi <batoolmalkawii@gmail.com>
2020-09-29 09:34:36 +0300

    edited padding

--- js/find.js
+++ js/find.js
@@ -52,7 +52,9 @@ function matchRequestToOffer(request) {
   }
 }
 function clearFindForm() {
-   findForm.textContent = '';
+  findForm.textContent = '';
+  let imgEl=document.getElementById('main-img');
+  imgEl.src='';
   let mainEl = document.getElementsByTagName('main')[0];
   console.log(mainEl);
   mainEl.style.backgroundImage = "url('../img/map.png')";
